5f94294e9909f8f6702519cdad2ea2c2caadcff4
Ahmed Ahres <ahres.ahmed@yahoo.fr>
2017-03-01 19:19:34 -0800

    Cleaner code

--- Handler.java
+++ Handler.java
@@ -6,38 +6,41 @@ import java.util.LinkedList;
 public class Handler {
 
 	LinkedList<GameObject> object = new LinkedList<GameObject>();
-	
-	public void tick(){
-		for(int i = 0; i < object.size(); i++) {
+
+	public void tick() {
+		for (int i = 0; i < object.size(); i++) {
 			GameObject tempObject = object.get(i);
-			
+
 			tempObject.tick();
 		}
 	}
-	public void clearEnemys(){
-	for(int i = 0; i<object.size();i++){
-		GameObject tempObject = object.get(i);
-		
-		if(tempObject.getId() != ID.Player) {
-			object.clear();
-			if(Game.gameState != Game.STATE.End){
-			addObject(new Player((int)tempObject.getX(),(int)tempObject.getY(), ID.Player, this));
-		}
-		}
-	}
-	}
-	public void render(Graphics g){
-		for(int i = 0; i < object.size(); i++){
+
+	public void clearEnemys() {
+		for (int i = 0; i < object.size(); i++) {
 			GameObject tempObject = object.get(i);
-			
+
+			if (tempObject.getId() != ID.Player) {
+				object.clear();
+				if (Game.gameState != Game.STATE.End) {
+					addObject(new Player((int) tempObject.getX(), (int) tempObject.getY(), ID.Player, this));
+				}
+			}
+		}
+	}
+
+	public void render(Graphics g) {
+		for (int i = 0; i < object.size(); i++) {
+			GameObject tempObject = object.get(i);
+
 			tempObject.render(g);
 		}
 	}
-	public void addObject(GameObject object){
+
+	public void addObject(GameObject object) {
 		this.object.add(object);
 	}
-	
-	public void removeObject(GameObject object){
+
+	public void removeObject(GameObject object) {
 		this.object.remove(object);
 	}
 }
